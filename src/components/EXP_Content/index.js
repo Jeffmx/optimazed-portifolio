5eedc21cbc7bb5fcb3d4e0755d36b962c11dfd15
Kyushi <54375059+Jeffmx@users.noreply.github.com>
2026-08-22 13:54:00 -0300

    Enhance image accessibility with alt attributes
    Added alt attributes to images for better accessibility.

--- src/components/EXP_Content/index.js
+++ src/components/EXP_Content/index.js
@@ -3,7 +3,7 @@ import "./EXP_Content.css"
 export const EXP_Nav = ({id, active, img, tittle, type}) =>{
   return(
     <li data-exp-target={`#${id}`} className={active}>
-      <img className="experiencia_nav_img" src={img}/>
+      <img className="experiencia_nav_img" src={img} alt={tittle}/>
       <div className="experiencia_nav_text">
         <h2>{tittle}</h2>
         <p>{type}</p>
@@ -19,10 +19,10 @@ export const EXP_Content = ({id, cover, tittle, desc, link, poster, active}) =>{
     <div className="experiencia_content_text">
       <h1>{tittle}</h1>
       <p dangerouslySetInnerHTML={{ __html: desc }}/>
-      <a href={link} target="_blank" className="content_text_button">Da uma olhada!</a>
+      <a href={link} target="_blank" rel="noreferrer" className="content_text_button">Da uma olhada!</a>
     </div>
     <div className="experiencia_content_poster">
-      <img className="poster_img" src={poster} />
+      <img className="poster_img" src={poster} alt=`Demo ${tittle}`/>
     </div>
   </section>
   )
